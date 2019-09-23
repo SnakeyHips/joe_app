3c2b6e2166d89563f0b0f144efe31bce520ed81f
Jake <super.jake@hotmail.co.uk>
2019-09-23 15:42:13 +0100

    home set up

--- src/components/Pages/Home.tsx
+++ src/components/Pages/Home.tsx
@@ -1,9 +1,12 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState, useEffect } from "react";
 import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 import Grid from "@material-ui/core/Grid";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import useStylesBase from "../../styles/styles-base";
 import clsx from "clsx";
+import { Photo } from "../../models/photo";
+import { GetAllPhotos } from "../../services/photo_service";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -13,14 +16,40 @@ const useStyles = makeStyles((theme: Theme) =>
 const Home: FunctionComponent = props => {
   const classes = useStyles({});
   const classesBase = useStylesBase({});
+  const [photos, setPhotos] = useState<Photo[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
   const smAndDown = useMediaQuery((theme: Theme) =>
     theme.breakpoints.down("sm")
+  );
+
+  useEffect(() => {
+    fetchPhotos();
+  }, [photos.length]);
+
+  async function fetchPhotos() {
+    setLoading(true);
+    const result: Photo[] = await GetAllPhotos();
+    if (result) {
+      setPhotos(result);
+    }
+    setLoading(false);
+  }
+
+  const content = loading ? (
+    <Grid container justify="center" >
+      <CircularProgress color="primary" />
+    </Grid>
+  ) : (
+    <>
+      <span>home</span>
+    </>
   );
 
   return (
     <div>
       <Grid container direction="column" justify="center">
-        <span>home</span>
+        {content}
       </Grid>
     </div>
   );
