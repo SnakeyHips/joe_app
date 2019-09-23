3b57197bb79ef27390bdb8f3be29f8aa44697fa4
Jake <super.jake@hotmail.co.uk>
2019-09-23 14:40:08 +0100

    added dotenv for imgur api

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./dotenv";
-import "./aws-amplify";
 import { ThemeProvider } from "@material-ui/styles";
 import { CssBaseline } from "@material-ui/core";
 import App from "./components/App";
