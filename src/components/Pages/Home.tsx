d141de88028d4b7fe4dc1966dcfcebb27b296f23
Jacob Gough <jacob.gough@amplyfigroup.com>
2020-03-11 08:59:30 +0000

    inf scrolling working

--- src/components/Pages/Home.tsx
+++ src/components/Pages/Home.tsx
@@ -30,7 +30,6 @@ export default function Home() {
   const classes = useStyles();
   const classesBase = useStylesBase();
   const [photos, setPhotos] = useState<Photo[]>([]);
-  const [infPhotos, setInfPhotos] = useState<Photo[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [page, setPage] = useState<number>(0);
   const [open, setOpen] = useState<boolean>(false);
@@ -45,7 +44,6 @@ export default function Home() {
     const result: Photo[] = await GetAllPhotos();
     if (result) {
       setPhotos(result);
-      setInfPhotos(result);
     }
     setLoading(false);
   }
@@ -61,7 +59,7 @@ export default function Home() {
 
   function handleLoadMore() {
     setPage(page + 1);
-    setInfPhotos([...photos.slice(0)]);
+    setPhotos([...photos, ...photos]);
   }
 
   const skeleton = <Skeleton key={0} className={classes.skeleton} variant="rect" height={582} width={874} />;
@@ -73,12 +71,7 @@ export default function Home() {
       {skeleton}
     </>
   ) : (
-    <InfiniteScroll
-      pageStart={page}
-      loadMore={handleLoadMore}
-      hasMore={photos.length < infPhotos.length}
-      loader={skeleton}
-    >
+    <InfiniteScroll pageStart={page} loadMore={handleLoadMore} hasMore={photos.length < 50}>
       {photos.map((photo: Photo) => {
         return (
           <Grid
@@ -102,11 +95,9 @@ export default function Home() {
   );
 
   return (
-    <div>
-      <Grid container direction="column" justify="center" alignItems="center">
-        {content}
-      </Grid>
+    <Grid container direction="column" justify="center" alignItems="center">
+      {content}
       {photo && <PhotoDialog open={open} handleClose={handleClose} photo={photo} />}
-    </div>
+    </Grid>
   );
 }
